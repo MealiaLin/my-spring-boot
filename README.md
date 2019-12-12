24f8db910dc8d6b34690d14f5c14d0d3614a3eea
Lin Yimin <mealie_lin@163.com>
2019-12-12 16:35:02 +0800

    集成swagger2文档

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Demo project for Spring Boot
     
 - 找到MySpringBootApplication 右键Run即可启动项目
 - 启动可直接访问http://localhost:8080/hello，  路径后缀是controller方法
+- 访问swagger文档路径是：http://localhost:8080/swagger-ui.html#/
+
+集成框架：
+
+- Spring Boot核心框架
+- Swagger 文档
--- src/main/java/com/example/myspringboot/controller/HelloWorldController.java
+++ src/main/java/com/example/myspringboot/controller/HelloWorldController.java
@@ -1,13 +1,17 @@
 package com.example.myspringboot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "hello world示范接口")
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloWorldController {
 
+    @ApiOperation("hello world")
     @GetMapping
     public String helloWorld(){
         return "Hello world";
